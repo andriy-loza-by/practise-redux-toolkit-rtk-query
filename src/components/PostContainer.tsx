0b9a93f39131c5cbb4b04eba48c22f767316d361
Andriy Loza <andriy.loza@blueyonder.com>
2022-01-19 11:24:12 +0200

    full RTK query with json-server

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {postAPI} from "../services/PostService";
 import PostItem from "./PostItem";
+import {IPost} from "../models/IPost";
 
 const PostContainer = () => {
     const [limit, setLimit] = useState(10)
-    const {data: posts, error, isLoading, refetch} = postAPI.useFetchAllPostsQuery(limit
-        // ,{pollingInterval: 2000}
-        );
+    const {data: posts, error, isLoading, refetch} = postAPI.useFetchAllPostsQuery(limit);
+    const [createPost, {}] = postAPI.useCreatePostMutation()
+    const [updatePost, {}] = postAPI.useUpdatePostMutation()
+    const [deletePost, {}] = postAPI.useDeletePostMutation()
 
     // useEffect(() => {
     //     setTimeout(() => {
@@ -14,14 +16,27 @@ const PostContainer = () => {
     //     }, 5000)
     // }, []);
 
+    const handleCreate = async () => {
+        const title = prompt()
+        await createPost({title, body: title} as IPost)
+    }
+
+    const handleRemove = (post: IPost) => {
+        deletePost(post)
+    }
+
+    const handleUpdate = (post: IPost) => {
+        updatePost(post)
+    }
+
     return (
         <div>
+            <button onClick={handleCreate}>Add new post</button>
             <div style={{border: '1px solid #efefef', padding: '20px'}}>
-                {/*<button onClick={() => refetch()}>RE-FETCH</button>*/}
                 {isLoading && <h1>Loading posts...</h1>}
                 {error && <h2>Error Loading posts</h2>}
                 {posts && posts.map(post =>
-                    <PostItem post={post} key={post.id}></PostItem>
+                    <PostItem remove={handleRemove} update={handleUpdate} post={post} key={post.id}></PostItem>
                 )}
             </div>
         </div>
